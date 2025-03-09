e38ad002eb49ed307ec4ff85b73e967f2c9b233f
Juan Felipe Restrepo Buitrago <jfrestrepb@eafit.edu.co>
2025-03-08 19:11:28 -0500

    Spring Boot Project Initialization

---
+++ src/main/java/com/eafit/HerzonTEIS/HerzonTeisApplication.java
@@ -0,0 +1,21 @@
+package com.eafit.HerzonTEIS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main class of the application
+ */
+@SpringBootApplication
+public class HerzonTeisApplication {
+
+	/**
+	 * Main method of the application
+	 * @param String[] args - Arguments of the main method
+	 * @return void
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(HerzonTeisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eafit/HerzonTEIS/HerzonTeisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eafit.HerzonTEIS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerzonTeisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
